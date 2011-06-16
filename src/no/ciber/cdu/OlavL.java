e5b85dccb64956f259319fd6b4477b23a71cf1ef
Olav Loen <olav.loen@ciber.com>
2011-06-16 14:29:56 +0200

    Mye rart

--- src/no/ciber/cdu/OlavL.java
+++ src/no/ciber/cdu/OlavL.java
@@ -3,12 +3,8 @@ package no.ciber.cdu;
 public class OlavL {
 
 	public static void main(String[] args) {
-		System.out.println("Lagt inn for Olav");
-<<<<<<< HEAD
-		System.out.println("Ny linje i remote repo. Nå? Litt mer tekst.");
-=======
-		System.out.println("Ny linje i remote repo");
 		System.out.println("ChristianL ruler");
->>>>>>> e9b5fc3e13d89c9c339508eafcdeffd4ff4d3349
+		System.out.println("Lagt inn for Olav");
+		System.out.println("Ny linje i remote repo. Nå? Litt mer tekst.");
 	}
 }
