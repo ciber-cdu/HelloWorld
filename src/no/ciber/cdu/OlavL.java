7b25b98042f5a8c30a15212546dc52f1c6fb7266
Christian Lundestad <christian.lundestad@ciber.com>
2011-06-16 14:28:38 +0200

    dfg

--- src/no/ciber/cdu/OlavL.java
+++ src/no/ciber/cdu/OlavL.java
@@ -5,6 +5,6 @@ public class OlavL {
 	public static void main(String[] args) {
 		System.out.println("Lagt inn for Olav");
 		System.out.println("Ny linje i remote repo");
-		System.out.println("ChristianL ruler");
+		System.out.println("ChristianL ruler. Yohoo!");
 	}
 }
